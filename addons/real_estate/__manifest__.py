4ced3312e0d26b43100c0080b8dc1ea3216c71d5
Dante Zulli <dantezulli2004@gmail.com>
2026-09-12 21:40:30 -0300

    feat: add real estate menus

--- addons/real_estate/__manifest__.py
+++ addons/real_estate/__manifest__.py
@@ -5,7 +5,10 @@
     "category": "Uncategorized",
     "version": "1.0",
     "depends": ["base"],
-    "data": ["views/estate_property_views.xml"],
+    "data": [
+        "views/estate_property_views.xml",
+        "views/real_estate_menuitem.xml",
+    ],
     "demo": [],
     "license": "LGPL-3",
     "installable": True,
