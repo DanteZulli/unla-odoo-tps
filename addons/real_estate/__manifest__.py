dd5b9f0727a2a0819cb90bec14d8b1c9d667aeba
Dante Zulli <dantezulli2004@gmail.com>
2026-09-12 21:55:29 -0300

    feat: define manager and salesman groups

--- addons/real_estate/__manifest__.py
+++ addons/real_estate/__manifest__.py
@@ -6,6 +6,7 @@
     "version": "1.0",
     "depends": ["base"],
     "data": [
+        "security/real_estate_res_groups.xml",
         "security/ir.model.access.csv",
         "views/estate_property_views.xml",
         "views/real_estate_menuitem.xml",
